8f49fda261172f5046b2f8ecde4f58f16fa0c8ae
Oleksandr Romaniuk <romanuksashko@gmail.com>
2026-08-21 11:23:43 +0300

    pr1

--- firstAngulat/src/app/app.ts
+++ firstAngulat/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header-component/header-component';
+import { NewsComponent } from './news-component/news-component';
+import { FooterComponent } from './footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NewsComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ firstAngulat/src/app/news-card-component/news-card-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewsCardComponent } from './news-card-component';
+
+describe('NewsCardComponent', () => {
+  let component: NewsCardComponent;
+  let fixture: ComponentFixture<NewsCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NewsCardComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NewsCardComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
